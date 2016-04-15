3f1cc21502dd26ced5933246cc186576291e0f58
coolassassin <coolstreetassassin@gmail.com>
2016-04-15 20:59:53 +0300

    Alpha Relise

--- js/control.js
+++ js/control.js
@@ -3,7 +3,8 @@
 	"use strict";
 	console.log('Инициализация приложения');
 	
-	var myMap;
+	var myMap,
+		mapClusterer;
 	ymaps.ready(init);
 
 	function init () { // init yandex map function
@@ -18,46 +19,264 @@
 			});
 			
 			YaPromise.then(function(output){
-				var coords = output.coords;
+				var coords = output.coords; // Geting coordinates
+				
+				// Map initializing
 				myMap = new ymaps.Map('map', {
 					center:[coords.latitude, coords.longitude],
-					zoom:12
+					zoom:15,
+					behaviors:['default', 'scrollZoom']
 				});
+				
+				destroyBut.classList.remove('hide');
+				
 				console.log('Ваши координаты: %f, %f', coords.latitude, coords.longitude);
 			}).then(function(e){
-				var myPlacemark1 = new ymaps.Placemark([59.873296200000006,30.988187399999997], {
+				var customItemContentLayout = ymaps.templateLayoutFactory.createClass(
+					// Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
+					'<h2 class=ballon_header>{{ properties.balloonContentHeader|raw }}</h2>' +
+					'<div class="ballon_body place-description">{{ properties.balloonContentBody|raw }}</div>' +
+					'<div class=ballon_footer><span class="pull-right">{{ properties.balloonContentFooter|raw }}</span></div>'
+				);
+				
+				mapClusterer = new ymaps.Clusterer({ // Claster init
+					clusterDisableClickZoom: true,
+					clusterOpenBalloonOnClick: true,
+					clusterBalloonContentLayout: 'cluster#balloonCarousel',
+					clusterBalloonItemContentLayout: customItemContentLayout,
+					clusterBalloonPanelMaxMapArea: 0,
+					clusterBalloonContentLayoutWidth: 400,
+					clusterBalloonContentLayoutHeight: 200,
+					clusterBalloonPagerSize: 5
+				});
+				
+				myMap.geoObjects.add(mapClusterer); // Add claster to map
+				
+				// Set events
+				myMap.events.add('click', function (e) {
+					var coords = e.get('coords'),		// get Click-map coordinates
+						modalPosition = e.get('clientPixels');  // get click-screen coordinates
+					ymaps.geocode(coords, { // Get Adress Row
+						results: 1
+					}).then(function(res){
+						var address = null;
+						if (res.geoObjects.get(0)){ // Open window if address founded
+							address = res.geoObjects.get(0).properties.get('text');
+							editFormTitle.innerHTML = address;
+							editForm.classList.remove('hide');
+							setElementPosition(editForm,modalPosition[0],modalPosition[1]);
+							currentPosition = {pos: coords, address: address};
+						}
+					});
+					
+				});
+			}).catch(function(e){
+				console.error(e);
+			});
+		}
+		
+		function setElementPosition (element,x,y) { // Set element position (@element - DOM object, @(x,y) - page coordinates)
+			if (x + element.offsetWidth <= window.innerWidth && x > 0) {
+				element.style.left = x + 'px';
+			} else {
+				if (x <= 0) {
+					element.style.left = 0 + 'px';
+				} else {
+					element.style.left = window.innerWidth - element.offsetWidth + 'px';
+				}
+			}
+			if (y + element.offsetHeight <= window.innerHeight && y > 0) {
+				element.style.top = y + 'px';
+			} else {
+				if (y <= 0) {
+					element.style.top = 0 + 'px';
+				} else {
+					element.style.top = window.innerHeight - element.offsetHeight + 'px';
+				}
+			}
+		}
+		
+		function setNewMark () {
+			var pasteIndex = null,
+				formInputs = Array.from(document.querySelectorAll('#fill_info_form .input_form .form-control')),
+				formValues = formInputs.map(t => t.value);
+			if (markArray.length) {
+				pasteIndex = markArray.findIndex(t => t.address == currentPosition.address);
+			}
+			if (pasteIndex == null || pasteIndex == -1) {
+				markArray.push({
+					address:currentPosition.address
+				});
+				pasteIndex = markArray.length - 1;
+			}
+			if (!markArray[pasteIndex].marks) {
+				markArray[pasteIndex].marks = [];
+			}
+			
+			var newMark = {
+				coords: {'x': currentPosition.pos[0], 'y' : currentPosition.pos[1]},
+				address: currentPosition.address,
+				name: formValues[0],
+				place: formValues[1],
+				text: formValues[2],
+				date: getCurrentTime()
+			};
+			
+			sendMark(newMark,function(e){
+				var result = JSON.parse(e);
+				if (result.error) {
+					console.error(result.error.message);
+					return false;
+				}
+				markArray[pasteIndex].marks.push(newMark);
+				
+				var myPlacemark = new ymaps.Placemark(currentPosition.pos, { // create new mark
 					iconContent: '1',
-					balloonContent: 'Балун',
+					balloonContentHeader: newMark.name,
+					balloonContentBody: '<div><a href="javascript:void(0);" class="address_link" data-index="'+ pasteIndex +'">' + newMark.address + '</a></div><div>' + newMark.text + '</div>',
+					balloonContentFooter: newMark.date,
 					hintContent: 'Стандартный значок метки'
 				}, 
 				{
 					preset: 'twirl#violetIcon'
 				});
 				
-				myMap.geoObjects.add(myPlacemark1);
-			}).catch(function(e){
-				console.error(e);
+				mapClusterer.add(myPlacemark); // Fill claster
+				
+				renderPlaceList(currentPosition.address);
 			});
+			
+			formInputs.forEach(t => t.value = '');
+		}
+		
+		function openFormByIndex (index,pos) {
+			var currentAddress = markArray[index];
+			console.log(index,currentAddress);
+			currentPosition = {pos: [currentAddress.marks[0].coords.x,currentAddress.marks[0].coords.y], address: currentAddress.address};
+			editFormTitle.innerHTML = currentPosition.address;
+			editForm.classList.remove('hide');
+			setElementPosition(editForm,pos[0] || 0,pos[1] || 0);
+		}
+		
+		function getCurrentTime () {
+			function doubleNumber (num) {
+				return num < 10 ? '0' + num : num;
+			}
+			var now = new Date();
+			return  now.getFullYear() + '.' + 
+					doubleNumber(now.getMonth()) + '.' + 
+					doubleNumber(now.getDate()) + ' ' + 
+					doubleNumber(now.getHours()) + ':' +
+					doubleNumber(now.getMinutes()) + ':' +
+					doubleNumber(now.getSeconds());
+		}
+		
+		function sendMark (obj, callback) {
+			var xhr = new XMLHttpRequest();
+
+			var json = JSON.stringify({"op" : "add","review" : obj});
+
+			xhr.open("POST", "http://localhost:3000/");
+
+			xhr.onreadystatechange = function(e){
+				if (xhr.readyState == 4) {
+					if (typeof callback == 'function') {
+						callback(xhr.responseText);
+					}
+				}
+			};
+
+			xhr.send(json);
+		}
+		
+		function renderPlaceList (address) {
+			var findIndex = null;
+			if (markArray.length) {
+				findIndex = markArray.findIndex(t => t.address == currentPosition.address);
+			}
+			if (findIndex != null && findIndex != -1) {
+				messageBox.innerHTML = markArray[findIndex].marks.map( t => renderListObject (t) ).join('');
+			}
+		}
+		
+		function renderListObject (obj) {
+			return '<div class="form-group-margin">'+
+					'<div><strong>'+ obj.name + '</strong> ' + obj.place + ' <span class="pull-right">' + obj.date + '</span></div><div class="place-description">' + obj.text + '</div>';
 		}
 		
 		// variables
-		var destroyBut = document.getElementById('destroyButton'),
-			loadBut = document.getElementById('loadButton');
+		var destroyBut = document.getElementById('destroyButton'), // Base buttons
+			loadBut = document.getElementById('loadButton'),
+			closeFormBut = document.querySelector('#fill_info_form .header .close'), // Form buttons
+			addNewMarkBut = document.getElementById('save_form_info'),
+			editForm = document.getElementById('fill_info_form'), // Form objects
+			editFormTitle = document.querySelector('#fill_info_form .header .title'),
+			messageBox = document.querySelector('#fill_info_form .message_box'),
+			markArray = [], // Init empty mark array
+			currentPosition = null, // values to paste mark
+			drag = 0, // drag values
+			dragHeader = null,
+			lastPosition = {
+				x : 0, 
+				y : 0
+			};
 		
 		// events declaration
-		destroyBut.addEventListener('click',function () {
+		destroyBut.addEventListener('click',function () { // destroy map by click
 			myMap.destroy();
 			this.classList.add('hide');
 			loadBut.classList.remove('hide');
-			
 		});
 		
-		loadBut.addEventListener('click',function () {
+		loadBut.addEventListener('click',function () { // load map by click
 			loadMap();
 			this.classList.add('hide');
 			destroyBut.classList.remove('hide');
 		});
 		
-		loadMap();
+		closeFormBut.addEventListener('click',function () { // close edit form
+			editForm.classList.add('hide');
+		});
+		
+		addNewMarkBut.addEventListener('click',function () { // close edit form
+			setNewMark();
+		});
+		
+		document.addEventListener('mousemove',function(e){ // DragNDrop form event
+			if (e.buttons == 1) {
+				if (!dragHeader) {
+					if (e.target.classList.contains('drag-header')) {
+						dragHeader = e.target;
+					} else if (e.target.closest('.drag-header')){
+						dragHeader = e.target.closest('.drag-header');
+					}
+				}
+				else {;
+					if (drag) {
+						setElementPosition(editForm,editForm.offsetLeft + (e.pageX - lastPosition.x),editForm.offsetTop + (e.pageY - lastPosition.y));
+					} else {
+						setElementPosition(editForm,editForm.offsetLeft,editForm.offsetTop);
+						drag = 1;
+					}
+					lastPosition.x = e.pageX;
+					lastPosition.y = e.pageY;
+				}
+			} else {
+				drag = 0;
+				dragHeader = null;
+			}
+		});
+		
+		document.addEventListener('click',function(e){
+			if (e.target.classList.contains('address_link')){
+				var baloon = e.target.closest('.ymaps-2-1-38-balloon-overlay');
+				console.log(baloon);
+				openFormByIndex(e.target.getAttribute('data-index'), [baloon.offsetLeft, baloon.offsetTop + baloon.offsetHeight + 10]);
+			}
+		});
+		
+		
+		
+		loadMap(); // on Load map init
 	}
 })();
--- production/bundle.js
+++ production/bundle.js
@@ -89,7 +89,7 @@
 
 
 	// module
-	exports.push([module.id, "body {\r\n    background: #fff;\r\n\theight: 100%;\r\n\tmargin: 0px;\r\n}\r\n\r\ndiv#map {\r\n\theight: 100% !important;\r\n\tbottom: 0px;\r\n\tposition: absolute;\r\n\toverflow: hidden;\r\n}\r\n\r\n#destroyButton {\r\n\tposition: fixed;\r\n\tright: 10px;\r\n\ttop: 10px;\r\n}\r\n\r\n#loadButton {\r\n\tposition: fixed;\r\n\tleft: 10px;\r\n\ttop: 10px;\r\n}\r\n", ""]);
+	exports.push([module.id, "body {\r\n    background: #fff;\r\n\theight: 100%;\r\n\tmargin: 0px;\r\n\tfont-family: \"Open Sans\",\"Helvetica Neue\",Helvetica,Arial,sans-serif;\r\n}\r\n\r\ndiv#map {\r\n\theight: 100% !important;\r\n\tbottom: 0px;\r\n\tposition: absolute;\r\n\toverflow: hidden;\r\n}\r\n\r\n#destroyButton {\r\n\tposition: fixed;\r\n\tleft: 10px;\r\n\tbottom: 40px;\r\n}\r\n\r\n#loadButton {\r\n\tposition: fixed;\r\n\tleft: 10px;\r\n\ttop: 10px;\r\n}\r\n\r\n#fill_info_form {\r\n\tz-index: 10;\r\n\tposition: fixed;\r\n\twidth: 500px;\r\n}\r\n\r\n#fill_info_form .header {\r\n\tcursor: move;\r\n}\r\n\r\n.form-group-margin {\r\n    margin-bottom: 15px;\r\n}\r\n\r\n.message_box {\r\n\theight: 100px;\r\n\toverflow: auto;\r\n}\r\n\r\n.message_box:empty::after {\r\n\tcontent: \"\\41E\\442\\437\\44B\\432\\44B   \\43E\\442\\441\\443\\442\\441\\442\\432\\443\\44E\\442\";\r\n\tfont-style: italic;\r\n}\r\n\r\ntextarea {\r\n\twidth: 100%;\r\n\tmin-width: 100%;\r\n}\r\n\r\n#fill_info_form .header .title {\r\n\toverflow: hidden;\r\n\twhite-space: nowrap;\r\n\ttext-overflow: ellipsis;\r\n\tdisplay: inline-block;\r\n\tmax-width: calc(100% - 25px);\r\n\tfont-size: 13px;\r\n\theight: 17px;\r\n\tmargin-bottom: -2px;\r\n}\r\n\r\n.place-description {\r\n\tfont-style: italic;\r\n}\r\n\r\n.ballon_body {\r\n\theight:100px;\r\n}", ""]);
 
 	// exports
 
@@ -411,7 +411,8 @@
 		"use strict";
 		console.log('Инициализация приложения');
 		
-		var myMap;
+		var myMap,
+			mapClusterer;
 		ymaps.ready(init);
 
 		function init () { // init yandex map function
@@ -426,47 +427,265 @@
 				});
 				
 				YaPromise.then(function(output){
-					var coords = output.coords;
+					var coords = output.coords; // Geting coordinates
+					
+					// Map initializing
 					myMap = new ymaps.Map('map', {
 						center:[coords.latitude, coords.longitude],
-						zoom:12
+						zoom:15,
+						behaviors:['default', 'scrollZoom']
 					});
+					
+					destroyBut.classList.remove('hide');
+					
 					console.log('Ваши координаты: %f, %f', coords.latitude, coords.longitude);
 				}).then(function(e){
-					var myPlacemark1 = new ymaps.Placemark([59.873296200000006,30.988187399999997], {
+					var customItemContentLayout = ymaps.templateLayoutFactory.createClass(
+						// Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
+						'<h2 class=ballon_header>{{ properties.balloonContentHeader|raw }}</h2>' +
+						'<div class="ballon_body place-description">{{ properties.balloonContentBody|raw }}</div>' +
+						'<div class=ballon_footer><span class="pull-right">{{ properties.balloonContentFooter|raw }}</span></div>'
+					);
+					
+					mapClusterer = new ymaps.Clusterer({ // Claster init
+						clusterDisableClickZoom: true,
+						clusterOpenBalloonOnClick: true,
+						clusterBalloonContentLayout: 'cluster#balloonCarousel',
+						clusterBalloonItemContentLayout: customItemContentLayout,
+						clusterBalloonPanelMaxMapArea: 0,
+						clusterBalloonContentLayoutWidth: 400,
+						clusterBalloonContentLayoutHeight: 200,
+						clusterBalloonPagerSize: 5
+					});
+					
+					myMap.geoObjects.add(mapClusterer); // Add claster to map
+					
+					// Set events
+					myMap.events.add('click', function (e) {
+						var coords = e.get('coords'),		// get Click-map coordinates
+							modalPosition = e.get('clientPixels');  // get click-screen coordinates
+						ymaps.geocode(coords, { // Get Adress Row
+							results: 1
+						}).then(function(res){
+							var address = null;
+							if (res.geoObjects.get(0)){ // Open window if address founded
+								address = res.geoObjects.get(0).properties.get('text');
+								editFormTitle.innerHTML = address;
+								editForm.classList.remove('hide');
+								setElementPosition(editForm,modalPosition[0],modalPosition[1]);
+								currentPosition = {pos: coords, address: address};
+							}
+						});
+						
+					});
+				}).catch(function(e){
+					console.error(e);
+				});
+			}
+			
+			function setElementPosition (element,x,y) { // Set element position (@element - DOM object, @(x,y) - page coordinates)
+				if (x + element.offsetWidth <= window.innerWidth && x > 0) {
+					element.style.left = x + 'px';
+				} else {
+					if (x <= 0) {
+						element.style.left = 0 + 'px';
+					} else {
+						element.style.left = window.innerWidth - element.offsetWidth + 'px';
+					}
+				}
+				if (y + element.offsetHeight <= window.innerHeight && y > 0) {
+					element.style.top = y + 'px';
+				} else {
+					if (y <= 0) {
+						element.style.top = 0 + 'px';
+					} else {
+						element.style.top = window.innerHeight - element.offsetHeight + 'px';
+					}
+				}
+			}
+			
+			function setNewMark () {
+				var pasteIndex = null,
+					formInputs = Array.from(document.querySelectorAll('#fill_info_form .input_form .form-control')),
+					formValues = formInputs.map(t => t.value);
+				if (markArray.length) {
+					pasteIndex = markArray.findIndex(t => t.address == currentPosition.address);
+				}
+				if (pasteIndex == null || pasteIndex == -1) {
+					markArray.push({
+						address:currentPosition.address
+					});
+					pasteIndex = markArray.length - 1;
+				}
+				if (!markArray[pasteIndex].marks) {
+					markArray[pasteIndex].marks = [];
+				}
+				
+				var newMark = {
+					coords: {'x': currentPosition.pos[0], 'y' : currentPosition.pos[1]},
+					address: currentPosition.address,
+					name: formValues[0],
+					place: formValues[1],
+					text: formValues[2],
+					date: getCurrentTime()
+				};
+				
+				sendMark(newMark,function(e){
+					var result = JSON.parse(e);
+					if (result.error) {
+						console.error(result.error.message);
+						return false;
+					}
+					markArray[pasteIndex].marks.push(newMark);
+					
+					var myPlacemark = new ymaps.Placemark(currentPosition.pos, { // create new mark
 						iconContent: '1',
-						balloonContent: 'Балун',
+						balloonContentHeader: newMark.name,
+						balloonContentBody: '<div><a href="javascript:void(0);" class="address_link" data-index="'+ pasteIndex +'">' + newMark.address + '</a></div><div>' + newMark.text + '</div>',
+						balloonContentFooter: newMark.date,
 						hintContent: 'Стандартный значок метки'
 					}, 
 					{
 						preset: 'twirl#violetIcon'
 					});
 					
-					myMap.geoObjects.add(myPlacemark1);
-				}).catch(function(e){
-					console.error(e);
+					mapClusterer.add(myPlacemark); // Fill claster
+					
+					renderPlaceList(currentPosition.address);
 				});
+				
+				formInputs.forEach(t => t.value = '');
+			}
+			
+			function openFormByIndex (index,pos) {
+				var currentAddress = markArray[index];
+				console.log(index,currentAddress);
+				currentPosition = {pos: [currentAddress.marks[0].coords.x,currentAddress.marks[0].coords.y], address: currentAddress.address};
+				editFormTitle.innerHTML = currentPosition.address;
+				editForm.classList.remove('hide');
+				setElementPosition(editForm,pos[0] || 0,pos[1] || 0);
+			}
+			
+			function getCurrentTime () {
+				function doubleNumber (num) {
+					return num < 10 ? '0' + num : num;
+				}
+				var now = new Date();
+				return  now.getFullYear() + '.' + 
+						doubleNumber(now.getMonth()) + '.' + 
+						doubleNumber(now.getDate()) + ' ' + 
+						doubleNumber(now.getHours()) + ':' +
+						doubleNumber(now.getMinutes()) + ':' +
+						doubleNumber(now.getSeconds());
+			}
+			
+			function sendMark (obj, callback) {
+				var xhr = new XMLHttpRequest();
+
+				var json = JSON.stringify({"op" : "add","review" : obj});
+
+				xhr.open("POST", "http://localhost:3000/");
+
+				xhr.onreadystatechange = function(e){
+					if (xhr.readyState == 4) {
+						if (typeof callback == 'function') {
+							callback(xhr.responseText);
+						}
+					}
+				};
+
+				xhr.send(json);
+			}
+			
+			function renderPlaceList (address) {
+				var findIndex = null;
+				if (markArray.length) {
+					findIndex = markArray.findIndex(t => t.address == currentPosition.address);
+				}
+				if (findIndex != null && findIndex != -1) {
+					messageBox.innerHTML = markArray[findIndex].marks.map( t => renderListObject (t) ).join('');
+				}
+			}
+			
+			function renderListObject (obj) {
+				return '<div class="form-group-margin">'+
+						'<div><strong>'+ obj.name + '</strong> ' + obj.place + ' <span class="pull-right">' + obj.date + '</span></div><div class="place-description">' + obj.text + '</div>';
 			}
 			
 			// variables
-			var destroyBut = document.getElementById('destroyButton'),
-				loadBut = document.getElementById('loadButton');
+			var destroyBut = document.getElementById('destroyButton'), // Base buttons
+				loadBut = document.getElementById('loadButton'),
+				closeFormBut = document.querySelector('#fill_info_form .header .close'), // Form buttons
+				addNewMarkBut = document.getElementById('save_form_info'),
+				editForm = document.getElementById('fill_info_form'), // Form objects
+				editFormTitle = document.querySelector('#fill_info_form .header .title'),
+				messageBox = document.querySelector('#fill_info_form .message_box'),
+				markArray = [], // Init empty mark array
+				currentPosition = null, // values to paste mark
+				drag = 0, // drag values
+				dragHeader = null,
+				lastPosition = {
+					x : 0, 
+					y : 0
+				};
 			
 			// events declaration
-			destroyBut.addEventListener('click',function () {
+			destroyBut.addEventListener('click',function () { // destroy map by click
 				myMap.destroy();
 				this.classList.add('hide');
 				loadBut.classList.remove('hide');
-				
 			});
 			
-			loadBut.addEventListener('click',function () {
+			loadBut.addEventListener('click',function () { // load map by click
 				loadMap();
 				this.classList.add('hide');
 				destroyBut.classList.remove('hide');
 			});
 			
-			loadMap();
+			closeFormBut.addEventListener('click',function () { // close edit form
+				editForm.classList.add('hide');
+			});
+			
+			addNewMarkBut.addEventListener('click',function () { // close edit form
+				setNewMark();
+			});
+			
+			document.addEventListener('mousemove',function(e){ // DragNDrop form event
+				if (e.buttons == 1) {
+					if (!dragHeader) {
+						if (e.target.classList.contains('drag-header')) {
+							dragHeader = e.target;
+						} else if (e.target.closest('.drag-header')){
+							dragHeader = e.target.closest('.drag-header');
+						}
+					}
+					else {;
+						if (drag) {
+							setElementPosition(editForm,editForm.offsetLeft + (e.pageX - lastPosition.x),editForm.offsetTop + (e.pageY - lastPosition.y));
+						} else {
+							setElementPosition(editForm,editForm.offsetLeft,editForm.offsetTop);
+							drag = 1;
+						}
+						lastPosition.x = e.pageX;
+						lastPosition.y = e.pageY;
+					}
+				} else {
+					drag = 0;
+					dragHeader = null;
+				}
+			});
+			
+			document.addEventListener('click',function(e){
+				if (e.target.classList.contains('address_link')){
+					var baloon = e.target.closest('.ymaps-2-1-38-balloon-overlay');
+					console.log(baloon);
+					openFormByIndex(e.target.getAttribute('data-index'), [baloon.offsetLeft, baloon.offsetTop + baloon.offsetHeight + 10]);
+				}
+			});
+			
+			
+			
+			loadMap(); // on Load map init
 		}
 	})();
 
